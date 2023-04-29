fc974a92a9bcbc830f00a3120fe1c6b461be1ba0
czl024 <czedl024@gmail.com>
2023-04-29 03:47:35 -0700

    added css for custom font

--- cinematic.js
+++ cinematic.js
@@ -1,7 +1,77 @@
-const WIDTH = 640;
-const HEIGHT = 480
+const WIDTH = 1080;
+const HEIGHT = 720;
+const DEBUG_WAIT = 2000;
+
+class Intro1 extends Phaser.Scene{
+    constructor(){super('i1');}
+
+    create(){
+        this.add.text(15, 15, "1", {fontFamily: 'bahn'});
+
+        setTimeout(() =>{
+            this.scene.start('i2');
+        }, DEBUG_WAIT);
+    }
+}
+
+class Intro2 extends Phaser.Scene{
+    constructor(){super('i2');}
+
+    create(){
+        this.add.text(15, 15, "2");
+
+        setTimeout(() =>{
+            this.scene.start('i3');
+        }, DEBUG_WAIT);
+    }
+}
+
+class Intro3 extends Phaser.Scene{
+    constructor(){super('i3');}
+
+    create(){
+        this.add.text(15, 15, "3");
+
+        setTimeout(() =>{
+            this.scene.start('i4');
+        }, DEBUG_WAIT);
+    }
+}
+
+class Intro4 extends Phaser.Scene{
+    constructor(){super('i4');}
+
+    create(){
+        this.add.text(15, 15, "4");
+
+        setTimeout(() =>{
+            this.scene.start('i5');
+        }, DEBUG_WAIT);
+    }
+}
+
+class Intro5 extends Phaser.Scene{
+    constructor(){super('i5');}
+
+    create(){
+        this.add.text(15, 15, "5");
+
+        setTimeout(() =>{
+            this.scene.start('title');
+        }, DEBUG_WAIT);
+    }
+}
+
+class Title extends Phaser.Scene{
+    constructor(){super('title');}
+
+    create(){
+        this.add.text(15, 15, "T");
+    }
+}
 
 new Phaser.Game({
     width: WIDTH,
-    height: HEIGHT
+    height: HEIGHT,
+    scene: [Intro1, Intro2, Intro3, Intro4, Intro5, Title]
 })
